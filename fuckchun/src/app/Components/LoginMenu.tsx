77b036fbe074795f0afe034c6670a0c7169cf32a
Foxiris <kirill-2202@yandex.ru>
2025-04-18 16:19:37 +0300

    Add login block

---
+++ fuckchun/src/app/Components/LoginMenu.tsx
@@ -0,0 +1,44 @@
+"use client"
+import React, {useState} from 'react';
+import Modal from 'react-modal';
+import {Dialog, DialogBackdrop, DialogPanel, DialogTitle} from "@headlessui/react";
+import {ExclamationTriangleIcon} from "@heroicons/react/24/outline";
+
+export default function LoginMenu() {
+    const [loginOpen, setLoginOpen] = useState(false);
+
+    const openLogin = () => {
+        setLoginOpen(true);
+    };
+    const closeLogin = () => {
+        setLoginOpen(false);
+    };
+
+    return (
+        <div className="flex">
+
+            <section className="absolute top-5 right-5 bg-black h-7 w-40 text-center rounded-lg">
+                <button onClick={openLogin}>Логин/Регистрация</button>
+            </section>
+            <Dialog open={loginOpen} onClose={setLoginOpen} className="relative z-">
+                <DialogBackdrop
+                    transition
+                    className="fixed inset-0 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+                />
+
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                        <DialogPanel
+                            transition
+                            className="h-100 rounded-lg opacity-40 bg-black text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-80 sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+                        >
+                            sosal
+                        </DialogPanel>
+                    </div>
+                </div>
+            </Dialog>
+
+        </div>
+
+    )
+}
--- fuckchun/src/app/page.tsx
+++ fuckchun/src/app/page.tsx
@@ -5,7 +5,7 @@ import Main2 from "./Components/Main2";
 export default function Home() {
   return (
 
-      <main className="bg-cyan-500 min-h-screen">
+      <main className="bg-cyan-500 min-h-screen flex flex-wrap">
         <Main/>
       </main>
 
